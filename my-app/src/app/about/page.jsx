a83be2d73fdefffff349b5fb3116304cf19eaa37
1234bhaskar <bhaskar2021chand@gmail.com>
2024-01-06 15:45:40 +0530

    working with images(internal image Source)

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -1,7 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>AboutPage</div>
+    <div className='flex justify-center mt-5'>
+      <div className='w-1/2 relative h-96 '>
+        <Image src={"/about.png"} fill/>
+
+        {/* external image */}
+        {/* <Image src="https://images.pexels.com/photos/18515836/pexels-photo-18515836/free-photo-of-portrait-of-a-starling-standing-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" fill/> */}
+      </div>
+    </div>
   )
 }
