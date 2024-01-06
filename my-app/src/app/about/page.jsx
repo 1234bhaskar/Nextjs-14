8a4c17da07b266aed7d7ff81ad588abf3ac57a40
1234bhaskar <bhaskar2021chand@gmail.com>
2024-01-06 15:46:27 +0530

    exgternal image Source

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -5,10 +5,10 @@ export default function page() {
   return (
     <div className='flex justify-center mt-5'>
       <div className='w-1/2 relative h-96 '>
-        <Image src={"/about.png"} fill/>
+        {/* <Image src={"/about.png"} fill/> */}
 
         {/* external image */}
-        {/* <Image src="https://images.pexels.com/photos/18515836/pexels-photo-18515836/free-photo-of-portrait-of-a-starling-standing-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" fill/> */}
+        <Image src="https://images.pexels.com/photos/18515836/pexels-photo-18515836/free-photo-of-portrait-of-a-starling-standing-outdoors.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" fill/>
       </div>
     </div>
   )
